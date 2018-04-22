28eed14cfb4abb2a30780990ed10770f1b17e9d5
Evgenyi Mason <em@branchup.pro>
2018-04-22 15:10:00 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# check-proxy-by-list
+# Скрипт для проверки прокси
+
+[![Latest Stable Version](https://poser.pugx.org/evmason/py-proxy-checker/v/stable)](https://packagist.org/packages/evmason/py-proxy-checker)
+[![Total Downloads](https://poser.pugx.org/evmason/py-proxy-checker/downloads)](https://packagist.org/packages/evmason/py-proxy-checker)
+[![Latest Unstable Version](https://poser.pugx.org/evmason/py-proxy-checker/v/unstable)](https://packagist.org/packages/evmason/py-proxy-checker)
+[![License](https://poser.pugx.org/evmason/py-proxy-checker/license)](https://packagist.org/packages/evmason/py-proxy-checker)
+
+Скрипт получает список прокси из txt файл, и производит проверку на работоспособность и анонимность прокси.
+
+## Использование
+
+### Пример файла proxy_list.txt:
+```
+# можно указывать диапазон IP адресов
+192.168.159.2:9011-192.168.159.4:9011
+
+# можно указывать конкретный IP адрес и порт
+192.168.159.10:9011
+
+# можно указывать с или без http:// в начале строки
+http://192.168.159.11:9011
+
+127.0.0.1:1111-127.0.0.3:1111
+```
+
+### Выполнение:
+если не указывать имя файла в командной строке, скрипт спросит его сразу после запуска
+ 
+```
+./check_proxy.py proxy_list.txt
+```
+
+### Результат выполнения:
+```
+http://192.168.159.2:9011 - OK
+http://192.168.159.3:9011 - OK
+http://192.168.159.4:9011 - OK
+http://192.168.159.10:9011 - OK
+http://192.168.159.11:9011 - OK
+http://127.0.0.1:1111 - Not works
+http://127.0.0.2:1111 - Not works
+http://127.0.0.3:1111 - Not works
+
+RESULT:
+OK proxies: 5
+Warning proxies: 0
+Bad proxies: 3
+```
+
+* `OK proxies` - кол. прокси которые прошли проверку и являются анонимными;
+* `Warning proxies` - кол. прокси которые передают в http заголовке настоящий IP адрес, и не являются анонимными;
+* `Bad proxies` - кол. рокси которые не отвечают.
+
+## Системные требования
+* python 3
--- check_proxy.py
+++ check_proxy.py
@@ -1,150 +1,160 @@
 #!/usr/bin/env python3
 
+import socket
 import urllib.request as req
 import urllib.error as req_e
 from urllib.parse import urlparse
 import re
 import os
-
-while True:
-	proxy_list_filename = input('Proxy list file name (Default: proxy_list.txt): ')
-
-	if proxy_list_filename == '':
-		proxy_list_filename = 'proxy_list.txt'
-
-	if os.path.isfile('proxy_list.txt') is False:
-		print('File not exists!')
-		continue
-
-	break
-
-class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+import sys
 
 def check_proxy(scheme, ip, port):
 	try:
-		proxy = req.ProxyHandler({scheme: '%s://%s:%s' % (scheme, ip, port)})
-		auth = req.HTTPBasicAuthHandler()
-		opener = req.build_opener(proxy, auth, req.HTTPHandler)
+		proxy_addr = '%s://%s:%s' % (scheme, ip, port)
+		req.urlcleanup()
+		proxy = req.ProxyHandler({'http': proxy_addr, 'https': proxy_addr})
+		opener = req.build_opener(proxy)
 		req.install_opener(opener)
-		conn = req.urlopen('http://stat.corbina.com.ua/')
+		conn = req.urlopen('https://branchup.pro/whatsmyip.php')
 		return conn.read()
-	except req_e.URLError:
+	except req_e.URLError as e:
 		return False
 
-counts = {
-	'error': 0,
-	'ok': 0,
-	'warn': 0,
-}
+if __name__ == '__main__':
+	counts = {
+			'error': 0,
+			'ok': 0,
+			'warn': 0,
+		}
 
-def get_scheme(address):
-	scheme = 'http'
-	try:
-		if address[:8] == 'https://':
-			scheme = 'https'
-	except:
-		pass
+	# set default timeout for all connections
+	socket.setdefaulttimeout(3)
 
-	return scheme
+	class bcolors:
+		HEADER = '\033[95m'
+		OKBLUE = '\033[94m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		FAIL = '\033[91m'
+		ENDC = '\033[0m'
+		BOLD = '\033[1m'
+		UNDERLINE = '\033[4m'
 
-def fix_address(address):
-	try:
-		if address[:7] != 'http://':
-			address = 'http://%s' % address
-	except:
-		pass
-
-	return address
-
-with open(proxy_list_filename) as proxy_list_file:
-	for line in proxy_list_file:
-		ips_list = []
-
-		line = fix_address(line)
-
+	def fix_address(address):
 		try:
-			line = line.strip()
-
-			if '-' in line:
-				ip_start, ip_end = line.split('-')
-				port = ''
-				o = urlparse(ip_start)
-				hostname_start = o.hostname
-				if o.scheme is not None:
-					scheme = o.scheme
-
-				if o.port is not None:
-					port = o.port
-
-
-				ip_end = fix_address(ip_end)
-				o = urlparse(ip_end)
-				hostname_end = o.hostname
-				if o.scheme is not None:
-					scheme = o.scheme
-
-				if o.port is not None:
-					port = o.port
-
-				if port is '':
-					continue
-
-				ip_parts = hostname_start.split('.')
-				ip_base = '%s.%s.%s.' % (ip_parts[0], ip_parts[1], ip_parts[2])
-				for i in range(int(hostname_start.split('.')[-1]), int(hostname_end.split('.')[-1]) + 1):
-					ips_list.append((scheme, '%s%s' % (ip_base, i), port))
-			else:
-				o = urlparse(line)
-
-				if o.port is None:
-					continue
-
-				port = o.port
-
-				scheme = 'http'
-				if o.scheme is not None:
-					scheme = o.scheme
-
-				hostname = o.hostname
-
-				ips_list = [(scheme, hostname, port)]
+			if address[:7] != 'http://':
+				address = 'http://%s' % address
 		except:
-			continue
+			pass
 
-		for scheme, ip, port in ips_list:
-			proxy_check_result = check_proxy(scheme, ip, port)
+		return address
 
-			proxy_check_result_txt = '%sNot works%s' % (bcolors.FAIL, bcolors.ENDC)
-			if proxy_check_result is not False:
-				# Your ip: 31.43.159.120
-				search_result = re.search('Your ip: (([0-9]{1,3}\.){3}[0-9]{1,3})', proxy_check_result.decode())
-				if search_result:
-					final_ip = search_result.group(1)
+	def main():
+		proxy_list_filename = None
 
-					if final_ip == ip:
-						proxy_check_result_txt = '%sOK%s' % (bcolors.OKGREEN, bcolors.ENDC)
-						counts['ok'] += 1
+		# check if proxy list file name passed in arguments
+		if len(sys.argv) > 1:
+			proxy_list_filename = sys.argv[1]
+
+		# check proxy list file exists
+		while True:
+			if proxy_list_filename is None:
+				proxy_list_filename = input('Proxy list file name (Default: proxy_list.txt): ')
+
+			if proxy_list_filename == '':
+				proxy_list_filename = 'proxy_list.txt'
+
+			if os.path.isfile('proxy_list.txt') is False:
+				print('File not exists!')
+				continue
+
+			break
+
+		with open(proxy_list_filename) as proxy_list_file:
+			for line in proxy_list_file:
+				ips_list = []
+
+				line = fix_address(line)
+
+				try:
+					line = line.strip()
+
+					if '-' in line:
+						ip_start, ip_end = line.split('-')
+						port = ''
+						o = urlparse(ip_start)
+						hostname_start = o.hostname
+						if o.scheme is not None:
+							scheme = o.scheme
+
+						if o.port is not None:
+							port = o.port
+
+
+						ip_end = fix_address(ip_end)
+						o = urlparse(ip_end)
+						hostname_end = o.hostname
+						if o.scheme is not None:
+							scheme = o.scheme
+
+						if o.port is not None:
+							port = o.port
+
+						if port is '':
+							continue
+
+						ip_parts = hostname_start.split('.')
+						ip_base = '%s.%s.%s.' % (ip_parts[0], ip_parts[1], ip_parts[2])
+						for i in range(int(hostname_start.split('.')[-1]), int(hostname_end.split('.')[-1]) + 1):
+							ips_list.append((scheme, '%s%s' % (ip_base, i), port))
 					else:
-						proxy_check_result_txt = '%sFinal IP not equal source%s' % (bcolors.WARNING, bcolors.ENDC)
-						counts['warn'] += 1
-				else:
-					proxy_check_result_txt = '%sService cant detect final IP%s' % (bcolors.WARNING, bcolors.ENDC)
-					counts['warn'] += 1
-			else:
-				counts['error'] += 1
+						o = urlparse(line)
+
+						if o.port is None:
+							continue
+
+						port = o.port
+
+						scheme = 'http'
+						if o.scheme is not None:
+							scheme = o.scheme
+
+						hostname = o.hostname
+
+						ips_list = [(scheme, hostname, port)]
+				except:
+					continue
+
+				for scheme, ip, port in ips_list:
+					proxy_check_result = check_proxy(scheme, ip, port)
+
+					proxy_check_result_txt = '%sNot works%s' % (bcolors.FAIL, bcolors.ENDC)
+					if proxy_check_result is not False:
+						search_result = re.search('(([0-9]{1,3}\.){3}[0-9]{1,3})', proxy_check_result.decode())
+						if search_result:
+							final_ip = search_result.group(1)
+
+							if final_ip == ip:
+								proxy_check_result_txt = '%sOK%s' % (bcolors.OKGREEN, bcolors.ENDC)
+								counts['ok'] += 1
+							else:
+								proxy_check_result_txt = '%sFinal IP not equal source%s' % (bcolors.WARNING, bcolors.ENDC)
+								counts['warn'] += 1
+						else:
+							proxy_check_result_txt = '%sService cant detect final IP%s' % (bcolors.WARNING, bcolors.ENDC)
+							counts['warn'] += 1
+					else:
+						counts['error'] += 1
 
 
-			print('%s://%s:%s - %s' % (scheme, ip, port, proxy_check_result_txt))
+					print('%s://%s:%s - %s' % (scheme, ip, port, proxy_check_result_txt))
 
-print('\n\nRESULT:')
-print('OK proxies:', counts['ok'])
-print('Warning proxies:', counts['warn'])
-print('Bad proxies:', counts['error'])
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit()
+	finally:
+		print('\n\nRESULT:')
+		print('OK proxies:', counts['ok'])
+		print('Warning proxies:', counts['warn'])
+		print('Bad proxies:', counts['error'])
